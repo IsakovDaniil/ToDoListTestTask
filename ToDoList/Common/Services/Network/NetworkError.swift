852e92c0346fef773527b7a706d0f0aea2cc98a8
IsakovDaniil <serverpgig@yandex.ru>
2026-03-09 20:33:48 +0300

    [Feature] add NetworkError

---
+++ ToDoList/Common/Services/Network/NetworkError.swift
@@ -0,0 +1,26 @@
+//
+//  Untitled.swift
+//  ToDoList
+//
+//  Created by Даниил on 09.03.2026.
+//
+
+import Foundation
+
+enum NetworkError: LocalizedError {
+    case invalidURL
+    case noData
+    case serverError(statusCode: Int)
+    case decodingFailed(Error)
+    case unowned(Error?)
+    
+    var errorDescription: String? {
+        switch self {
+        case .invalidURL: "Неверный URL"
+        case .noData: "Нет данных от сервера"
+        case .serverError(let code): "Ошибка сервера: \(code)"
+        case .decodingFailed(let error): "Ошибка декодирования: \(error.localizedDescription)"
+        case .unowned(let error): error?.localizedDescription ?? "Неизвестная ошибка"
+        }
+    }
+}
