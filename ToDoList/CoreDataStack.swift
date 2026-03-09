08087d949b02bbeed3c91ff205b5b6567a7572c6
IsakovDaniil <serverpgig@yandex.ru>
2026-03-09 23:26:19 +0300

    [Feature] add CoreDataStack

---
+++ ToDoList/CoreDataStack.swift
@@ -0,0 +1,38 @@
+//
+//  CoreDataStack.swift
+//  ToDoList
+//
+//  Created by Даниил on 09.03.2026.
+//
+
+import CoreData
+
+final class CoreDataStack {
+    
+    // MARK: - Singleton
+    
+    static let shared = CoreDataStack()
+    
+    private init() {}
+    
+    lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "ToDoList")
+        container.loadPersistentStores { _, error in
+            if let error {
+                fatalError("CoreData не загрузился: \(error.localizedDescription)")
+            }
+        }
+        container.viewContext.automaticallyMergesChangesFromParent = true
+        return container
+    }()
+    
+    // MARK: - Contexts
+    
+    var mainContext: NSManagedObjectContext {
+        persistentContainer.viewContext
+    }
+    
+    func newBackgroundContext() -> NSManagedObjectContext {
+        persistentContainer.newBackgroundContext()
+    }
+}
