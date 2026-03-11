43157689980882dad8742cdb7c33c8eb1215fded
IsakovDaniil <serverpgig@yandex.ru>
2026-03-11 16:50:11 +0300

    [Feature] add TasksViewController with navigation bar

--- ToDoList/Modules/Tasks/TasksViewController.swift
+++ ToDoList/Modules/Tasks/TasksViewController.swift
@@ -13,14 +13,42 @@ protocol TasksViewProtocol: AnyObject {
 
 final class TasksViewController: UIViewController {
     
+    // MARK: - Constants
+    
+    private enum Constants {
+        static let title = "Задачи"
+    }
+    
+    // MARK: - Properties
+    
     var presenter: TasksPresenterProtocol!
 
+    // MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        
+        setupView()
     }
-
+    
+    // MARK: - Setup
+    
+    private func setupView() {
+        view.backgroundColor = .appBlack
+        setupNavigationBar()
+    }
+    
+    private func setupNavigationBar() {
+        title = Constants.title
+        
+        let titleAttributes: [NSAttributedString.Key: Any] = [
+            .foregroundColor: UIColor.white
+        ]
+        
+        navigationController?.navigationBar.largeTitleTextAttributes = titleAttributes
+        navigationController?.navigationBar.titleTextAttributes = titleAttributes
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.largeTitleDisplayMode = .always
+    }
 
 }
 
