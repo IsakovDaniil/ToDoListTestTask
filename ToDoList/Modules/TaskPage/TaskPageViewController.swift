4db0f869c76510b38ac1aea7e28ab9f32ab8f4fa
IsakovDaniil <serverpgig@yandex.ru>
2026-03-12 17:21:48 +0300

    [Feature] add TaskPage UI with title, date and description

--- ToDoList/Modules/TaskPage/TaskPageViewController.swift
+++ ToDoList/Modules/TaskPage/TaskPageViewController.swift
@@ -15,19 +15,62 @@ final class TaskPageViewController: UIViewController {
     
     // MARK: - Constants
     
-    enum Constants {
-        
+    private enum Constants {
+        static let TextFieldPlaceholder = "Название"
+        static let horizontalPadding: CGFloat = 20
+        static let titleFontSize: CGFloat = 34
+        static let dateFontSize: CGFloat = 12
+        static let descriptionFontSize: CGFloat = 16
+        static let titleTopPadding: CGFloat = 8
+        static let dateTopPadding: CGFloat = 8
+        static let descriptionTopPadding: CGFloat = 16
     }
     
     // MARK: - Properties
     
     var presenter: TaskPagePresenterProtocol?
     
+    // MARK: - UIElements
+    
+    private lazy var titleTextField: UITextField = {
+        let textField = UITextField()
+        textField.font = .systemFont(ofSize: Constants.titleFontSize, weight: .bold)
+        textField.textColor = .appWhite
+        textField.placeholder = Constants.TextFieldPlaceholder
+        textField.borderStyle = .none
+        textField.returnKeyType = .done
+        textField.delegate = self
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
+    
+    private var dateLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: Constants.dateFontSize, weight: .regular)
+        label.textColor = .appWhite.withAlphaComponent(0.5)
+        label.text = DateHelper.format(Date())
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private var descriptionTextView: UITextView = {
+        let textView = UITextView()
+        textView.font = .systemFont(ofSize: Constants.descriptionFontSize, weight: .regular)
+        textView.textColor = .appWhite
+        textView.backgroundColor = .clear
+        textView.isScrollEnabled = true
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        return textView
+    }()
+    
     // MARK: - Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupView()
+        setupConstraints()
         navigationController?.isToolbarHidden = true
+        navigationController?.navigationBar.prefersLargeTitles = false
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -39,12 +82,38 @@ final class TaskPageViewController: UIViewController {
     
     private func setupView() {
         view.backgroundColor = .appBlack
+        view.addSubview(titleTextField)
+        view.addSubview(dateLabel)
+        view.addSubview(descriptionTextView)
         
     }
     
-    private func setupNavigationBar() {
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            titleTextField.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: Constants.titleTopPadding),
+            titleTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: Constants.horizontalPadding),
+            titleTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -Constants.horizontalPadding),
+            
+            dateLabel.topAnchor.constraint(equalTo: titleTextField.bottomAnchor, constant: Constants.dateTopPadding),
+            dateLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: Constants.horizontalPadding),
+            
+            descriptionTextView.topAnchor.constraint(equalTo: dateLabel.bottomAnchor, constant: Constants.descriptionTopPadding),
+            descriptionTextView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: Constants.horizontalPadding),
+            descriptionTextView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -Constants.horizontalPadding),
+            descriptionTextView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
     }
     
+}
+
+// MARK: - UITextFieldDelegate
+
+extension TaskPageViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        descriptionTextView.becomeFirstResponder()
+        return true
+    }
 }
 
 extension TaskPageViewController: TaskPageViewProtocol {
